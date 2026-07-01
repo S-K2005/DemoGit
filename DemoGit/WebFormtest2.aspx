90de2bd309b1f7bc9c7a44ac0fe9e9b5bda29380
Akshatadhale <akshatadhale009@gmail.com>
2026-07-01 09:34:07 +0530

    Second Test

---
+++ DemoGit/WebFormtest2.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormtest2.aspx.cs" Inherits="DemoGit.WebFormtest2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+
+        </div>
+    </form>
+</body>
+</html>
--- DemoGit/WebFormtest2.aspx.designer.cs
+++ DemoGit/WebFormtest2.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace DemoGit
 {
 
 
-    public partial class first
+    public partial class WebFormtest2
     {
 
         /// <summary>
@@ -31,23 +31,5 @@ namespace DemoGit
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
-
-        /// <summary>
-        /// Button2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
-
-        /// <summary>
-        /// Button3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button3;
     }
 }
